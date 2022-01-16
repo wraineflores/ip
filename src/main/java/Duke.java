51dc8d7cfd98b594d4cacb18beeaa025abc76809
wraineflores <wraineflores@gmail.com>
2022-01-16 14:52:02 +0800

    First time commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        String statement = "Hello! I'm Duke";
+        String question = "What can I do for you?";
+        System.out.println(statement);
+        System.out.println(question);
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
     }
 }
