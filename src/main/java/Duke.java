2d7285e7b65f13b0e225770ddde919c8af224049
wraineflores <wraineflores@gmail.com>
2022-01-16 14:56:42 +0800

    Second time commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,10 @@ public class Duke {
     public static void main(String[] args) {
         String statement = "Hello! I'm Duke";
         String question = "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
         System.out.println(statement);
         System.out.println(question);
+        System.out.println(farewell);
         /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
