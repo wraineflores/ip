60fba685d3eceb052af7572e7db05677441ff372
wraineflores <wraineflores@gmail.com>
2022-01-23 17:16:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DM_DEFAULT_ENCODING")
     public static void main(String[] args) {
-        String statement = "Hello! I'm Duke";
-        String question = "What can I do for you?";
-        String farewell = "Bye. Hope to see you again soon!";
-        System.out.println(statement);
-        System.out.println(question);
-        System.out.println(farewell);
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
+        System.out.println("Hello! I'm Earl Grey\nWhat can I do for you?");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while (!"bye".equals(line)) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
+
+        System.out.println("Bye. Until we meet again!");
     }
 }
