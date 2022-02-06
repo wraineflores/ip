ee1540241076a2ac6344a956bd979b4e5bbb99bc
wraineflores <wraineflores@gmail.com>
2022-02-06 09:29:15 +0800

    master-Level-5-v1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     //@edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DM_DEFAULT_ENCODING")
     public static void main(String[] args) {
         System.out.println("Hello! I'm Earl Grey\nWhat can I do for you?");
+        // test master commit comment
 
         String line;
         Scanner in = new Scanner(System.in);
