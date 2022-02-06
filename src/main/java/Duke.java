778e3576f657bef48fdeb9b34dd2f8aef4763dbd
wraineflores <wraineflores@gmail.com>
2022-02-06 09:25:58 +0800

    branch-Level-5-v1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     //@edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DM_DEFAULT_ENCODING")
     public static void main(String[] args) {
         System.out.println("Hello! I'm Earl Grey\nWhat can I do for you?");
+        // Branching test comment
 
         String line;
         Scanner in = new Scanner(System.in);
