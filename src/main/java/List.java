592ea195276c491436bd8b9654306f3be24813a9
wraineflores <wraineflores@gmail.com>
2022-02-12 14:27:47 +0800

    Level-6 made a delete function in the code base

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,17 +1,20 @@
-public class List {
-    protected Task[] addLists;
+import java.util.ArrayList;
 
-    public List(Task[] addLists) {
+public class List {
+    //protected static Object[] addLists;
+    protected static ArrayList<Task> addLists;
+
+    protected List(ArrayList<Task> addLists) {
         this.addLists = addLists;
     }
 
-    public void printList() {
+    static void printList() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < addLists.length; i++) {
-            if (addLists[i] == null) {
-                break;
+        for (int i = 0; i < addLists.size(); i++) {
+            if (addLists.get(i) == null) {
+                continue;
             } else {
-                System.out.println(Integer.toString(i + 1) + "." + addLists[i].toString().trim());
+                System.out.println(Integer.toString(i + 1) + "." + addLists.get(i).toString().trim());
             }
         }
     }
